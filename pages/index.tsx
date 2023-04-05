00477dfd13e071b2d3bed61637b7d9a5533567f5
shamim-5 <shamim.somadder5@gmail.com>
2023-04-05 14:47:10 +0600

    added custom navbar with scroll effect change bg-color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,15 +20,23 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
   return (
     <>
-      <h1 className="text-4xl text-green-400">Netflix Clone</h1>
-      <p className="text-white">Logged in as: {user?.email}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout!
-      </button>
+      <Navbar />
+      <div className="bg-gray-500">
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+       
+      </div>
     </>
   );
 }
